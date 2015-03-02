7c05fdf622781ca0895499419efd9861e5d47de4
Justin Lin <caterpillar@openhome.cc>
2015-03-02 09:59:49 +0800

    取得指定 Tag 內容

--- src/cc/openhome/PageMapper.java
+++ src/cc/openhome/PageMapper.java
@@ -6,7 +6,11 @@ import static java.lang.System.out;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import static java.util.stream.Collectors.toList;
 import java.util.stream.Stream;
 
@@ -15,14 +19,24 @@ import java.util.stream.Stream;
  * @author Justin
  */
 public class PageMapper {
-
+    private static String template = fileContent(Paths.get("c:\\workspace\\template.html"));
+    
+    private static Map<String, Pattern> patterns = new HashMap<>();
+    static {
+        patterns.put("title", Pattern.compile("<title>(.*)</title>"));
+        patterns.put("body", Pattern.compile("<body>((.*\\s*)*)</body>"));
+    }    
+            
     public static void main(String[] args) {
         List<String> htmlFiles = htmlFiles(Paths.get("c:\\workspace\\Java\\"));
-        out.println(htmlFiles);
-        out.println(htmlFiles.stream()
+        String content = htmlFiles.stream()
                 .map(Paths::get)
                 .map(PageMapper::fileContent)
-                .findFirst().get());
+                .findFirst().get();
+        
+        out.println(tagContent(content, "title"));
+        out.println(tagContent(content, "body"));
+        
 
     }
 
@@ -47,4 +61,11 @@ public class PageMapper {
             throw new UncheckedIOException(ex);
         }
     }
+    
+    private static String tagContent(String content, String tag) {
+        Matcher matcher =  patterns.get(tag).matcher(content);
+        matcher.find();
+        return matcher.group(1);
+    }
+   
 }
