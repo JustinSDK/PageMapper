6af31a2cf57ed992c48aec36828ca452ce0d16bd
Justin Lin <caterpillar@openhome.cc>
2015-03-02 08:46:32 +0800

    處理 Checked IO 為 Unchecked IO

--- src/cc/openhome/PageMapper.java
+++ src/cc/openhome/PageMapper.java
@@ -6,10 +6,7 @@ import static java.lang.System.out;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import static java.util.Collections.emptyList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import static java.util.stream.Collectors.toList;
 import java.util.stream.Stream;
 
@@ -18,33 +15,35 @@ import java.util.stream.Stream;
  * @author Justin
  */
 public class PageMapper {
-    public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args) {
         List<String> htmlFiles = htmlFiles("c:\\workspace\\Java\\");
         out.println(htmlFiles);
         out.println(htmlFiles.stream()
                 .map(PageMapper::fileContent)
                 .findFirst().get());
-                
-                
-                
+
     }
 
-    private static List<String> htmlFiles(String dir) throws IOException {
-        try(Stream<Path> paths = Files.list(Paths.get(dir))) {
+    private static List<String> htmlFiles(String dir) {
+        try (Stream<Path> paths = withIO(() -> Files.list(Paths.get(dir)))) {
             return paths.map(Path::toString)
-                       .filter(str -> str.endsWith(".html"))
-                       .collect(toList());            
+                    .filter(str -> str.endsWith(".html"))
+                    .collect(toList());
         }
     }
-    
+
     private static String fileContent(String dir) {
+        return withIO(() -> Files.readAllLines(Paths.get(dir)).stream()
+                .reduce((acc, line) -> acc + line + System.getProperty("line.separator"))
+                .get());
+    }
+
+    private static <R> R withIO(UncheckedIO<R> io) {
         try {
-            return Files.readAllLines(Paths.get(dir)).stream()
-                      .reduce((acc, line) -> acc + line + System.getProperty("line.separator"))
-                      .get();
+            return io.run();
         } catch (IOException ex) {
             throw new UncheckedIOException(ex);
         }
     }
-    
 }
