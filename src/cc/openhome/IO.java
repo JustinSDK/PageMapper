e69d144abbded4145047b1dea1924acb22d9e5c5
Justin Lin <caterpillar@openhome.cc>
2015-03-02 11:39:07 +0800

    將 IO 動作整理至 IO 類別

---
+++ src/cc/openhome/IO.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cc.openhome;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import static java.util.stream.Collectors.toList;
+import java.util.stream.Stream;
+
+/**
+ *
+ * @author Justin
+ */
+public class IO {
+
+    public static List htmlFiles(Path path) {
+        try (final Stream<Path> paths = withIO(() -> Files.list(path))) {
+            return paths.map(Path::toString).filter((String str) -> str.endsWith(".html")).collect(toList());
+        }
+    }
+
+    public static <R> R withIO(UncheckedIO<R> io) {
+        try {
+            return io.run();
+        } catch (IOException ex) {
+            throw new UncheckedIOException(ex);
+        }
+    }
+
+    public static PathContent pathContent(Path path) {
+        String content = withIO(() -> Files.readAllLines(path).stream().reduce((String acc, String line) -> acc + line + System.getProperty("line.separator")).get());
+        return new PathContent(path, content);
+    }
+}
--- src/cc/openhome/PageMapper.java
+++ src/cc/openhome/PageMapper.java
@@ -1,8 +1,6 @@
 package cc.openhome;
 
-import java.io.IOException;
-import java.io.UncheckedIOException;
-import static java.lang.System.out;
+import static cc.openhome.IO.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -13,8 +11,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import static java.util.stream.Collectors.toList;
-import java.util.stream.Stream;
 
 /**
  *
@@ -35,36 +31,12 @@ public class PageMapper {
                 .map(Paths::get)
                 // 排除首頁，因為比較複雜，要手動修改
                 .filter(path -> !path.getFileName().toString().equals("index.html"))
-                .map(PageMapper::pathContent)
+                .map(IO::pathContent)
                 // 處理 div class="article" 與 title
                 .map(PageMapper::map)
                 .forEach(pathContent -> {
-                    //withIO(() -> Files.write(pathContent.path, pathContent.content.getBytes("UTF-8"), TRUNCATE_EXISTING, CREATE));
+                     //withIO(() -> Files.write(pathContent.path, pathContent.content.getBytes("UTF-8"), TRUNCATE_EXISTING, CREATE));
                 });
-        
-    }
-
-    private static List<String> htmlFiles(Path path) {
-        try (Stream<Path> paths = withIO(() -> Files.list(path))) {
-            return paths.map(Path::toString)
-                     .filter(str -> str.endsWith(".html"))
-                     .collect(toList());
-        }
-    }
-
-    private static PathContent pathContent(Path path) {
-        String content = withIO(() -> Files.readAllLines(path).stream()
-                .reduce((acc, line) -> acc + line + System.getProperty("line.separator"))
-                .get());
-        return new PathContent(path, content);
-    }
-
-    private static <R> R withIO(UncheckedIO<R> io) {
-        try {
-            return io.run();
-        } catch (IOException ex) {
-            throw new UncheckedIOException(ex);
-        }
     }
     
     private static String tagContent(String content, String tag) {
