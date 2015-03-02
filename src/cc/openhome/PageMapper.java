4e8e7d6cd038738605c495667c3b0a8d4eb4bd9d
Justin Lin <caterpillar@openhome.cc>
2015-03-02 14:27:15 +0800

    重用 Regex

--- src/cc/openhome/PageMapper.java
+++ src/cc/openhome/PageMapper.java
@@ -20,20 +20,21 @@ public class PageMapper {
     static {
         patterns.put("title", Pattern.compile("<title>(.*)</title>"));
         patterns.put("div class=\"article\"", Pattern.compile("<div class=\"article\">((.*\\s*)*?)</div>"));
+        patterns.put("stripTags", Pattern.compile("\\<[^>]*>"));
     }    
             
     public static void main(String[] args) {
-        List<String> htmlFiles = htmlFiles(Paths.get("c:\\workspace\\NewJava\\"));
-        htmlFiles.stream()
-                .map(Paths::get)
-                // 排除首頁，因為比較複雜，要手動修改
-                .filter(path -> !path.getFileName().toString().equals("index.html"))
-                .map(IO::pathContent)
-                // 處理 div class="article" 與 title
-                .map(PageMapper::map2Template)
-                .forEach(IO::write);
-//        String content = pathContent(Paths.get("c:\\workspace\\Java\\Abstract.html")).content;
-//        out.println(tagContent(content, "div class=\"article\"").replaceAll("\\<[^>]*>", "").trim().substring(0, 140) + ".."); 
+//        List<String> htmlFiles = htmlFiles(Paths.get("c:\\workspace\\NewJava\\"));
+//        htmlFiles.stream()
+//                .map(Paths::get)
+//                // 排除首頁，因為比較複雜，要手動修改
+//                .filter(path -> !path.getFileName().toString().equals("index.html"))
+//                .map(IO::pathContent)
+//                // 處理 div class="article" 與 title
+//                .map(PageMapper::map2Template)
+//                .forEach(IO::write);
+          String content = tagContent(pathContent(Paths.get("c:\\workspace\\Java\\Abstract.html")).content, "div class=\"article\"");
+          out.println(patterns.get("stripTags").matcher(content).replaceAll("").trim().substring(0, 100) + "...");
     }
     
     private static String tagContent(String content, String tag) {
@@ -44,12 +45,13 @@ public class PageMapper {
      
     private static PathContent map2Template(PathContent pathContent) {
         String content = tagContent(pathContent.content, "div class=\"article\"");
+        
          pathContent.content = 
              template.content
                    .replace("#content#", content)
                    .replaceAll("#title#", tagContent(pathContent.content, "title"))
                    .replaceAll("#fileName#", pathContent.path.getFileName().toString())
-                   .replaceAll("#description#", content.replaceAll("\\<[^>]*>", "").trim().substring(0, 140) + "...");
+                   .replaceAll("#description#", patterns.get("stripTags").matcher(content).replaceAll("").trim().substring(0, 100) + "...");
          return pathContent;
     }
 }
