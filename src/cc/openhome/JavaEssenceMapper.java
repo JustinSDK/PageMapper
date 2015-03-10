91901f874a3ff47fa2699643baaa2d8b92e0573a
Justin Lin <caterpillar@openhome.cc>
2015-03-10 20:27:11 +0800

    完成 Java Essence 的處理

---
+++ src/cc/openhome/JavaEssenceMapper.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cc.openhome;
+
+import static cc.openhome.IO.htmlFiles;
+import java.nio.charset.Charset;
+import java.nio.file.Paths;
+import java.util.List;
+
+/**
+ *
+ * @author Justin
+ */
+public class JavaEssenceMapper {
+        public static void main(String[] args) {
+        List<String> htmlFiles = htmlFiles(Paths.get("c:\\workspace\\JavaEssence\\"));
+        htmlFiles.stream()
+                .map(Paths::get)
+                // 排除首頁，因為比較複雜，要手動修改
+                .filter(path -> !path.getFileName().toString().equals("index.html"))
+                .map(path -> IO.pathContent(path, Charset.forName("Big5")))
+                .map(pathContent -> PageMapper.titleTdArticle2Template(pathContent, "tdStudyGossip"))
+                .map(PageMapper::img2RWD)
+                .map(pathContent -> PageMapper.pre2PrettyPrint(pathContent, "java"))
+                .forEach(IO::write);
+    }
+}
