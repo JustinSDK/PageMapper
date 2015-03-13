dfb42dbc7d43205c83ac025a5bbadaf9b8d0d154
Justin Lin <caterpillar@openhome.cc>
2015-03-13 08:52:19 +0800

    完成 Qt4Gossip

---
+++ src/cc/openhome/specific/Qt4Mapper.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cc.openhome.specific;
+
+import cc.openhome.IO;
+import cc.openhome.PageMapper;
+import static cc.openhome.IO.htmlFiles;
+import java.nio.charset.Charset;
+import java.nio.file.Paths;
+import java.util.List;
+
+/**
+ *
+ * @author Justin
+ */
+public class Qt4Mapper {
+        public static void main(String[] args) {
+        List<String> htmlFiles = htmlFiles(Paths.get("c:\\workspace\\Qt4Gossip\\"));
+        htmlFiles.stream()
+                .map(Paths::get)
+                // 排除首頁，因為比較複雜，要手動修改
+                .filter(path -> !path.getFileName().toString().equals("index.html"))
+                .map(path -> IO.pathContent(path, Charset.forName("Big5")))
+                .map(PageMapper::clean)
+                .map(pathContent -> PageMapper.titleTdArticle2Template(pathContent, "tdAlgorithmGossip"))
+                .map(PageMapper::img2RWD)
+                .map(pathContent -> PageMapper.pre2PrettyPrint(pathContent, "c++"))
+                .forEach(IO::write);
+    }
+}
